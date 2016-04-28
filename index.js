ec330de3d901e677fed4523ff91d5aed5c39d3ad
Chris Heelan <cjh276@cornell.edu>
2016-04-27 22:55:16 -0700

    Respond to messages with echo

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var request = require("request");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,12 +12,51 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'test_verify') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
+app.post('/', jsonParser, function(req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var token = "CAANqdZBOAPUEBADZApJ5w89otf4QoT8B2h9wUNAgMOq4lMSj9iKEwLZAMapQWGZCsM4OF6XwX1bqd1e7ZCZANQ5ERv7HNzPo8Yn51LPz2BFsS3QZChqRgaVsLLcwbBUUvMqQEFD11kEZAWaVgb88YnQtsvuflrnOGQNTD0f1ExOO9WHXsb1T0XhiFhMZB9ga9MscZD";
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 
